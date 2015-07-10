62c4dd99a51353ec55d32b0087f3b90e24a8ab9f
Richard West <r.west@neu.edu>
2015-07-10 18:35:31 -0400

    Trying to fix the LD_LIBRARY_PATH hack for mopac

--- mopac/mopac.sh
+++ mopac/mopac.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # A wrapper to set up the environment and call MOPAC2012.exe
-MOPAC_LICENSE=/opt/anaconda1anaconda2anaconda3/opt/mopac/ 
-LD_LIBRARY_PATH=$MOPAC_LICENSE
-/opt/anaconda1anaconda2anaconda3/opt/mopac/MOPAC2012.exe "$@"
+MOPAC_LICENSE=/opt/anaconda1anaconda2anaconda3/opt/mopac/ LD_LIBRARY_PATH=/opt/anaconda1anaconda2anaconda3/opt/mopac/ /opt/anaconda1anaconda2anaconda3/opt/mopac/MOPAC2012.exe "$@"
