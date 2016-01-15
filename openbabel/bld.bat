a98f0273916e0e94becf2bda4741ce631dec5e4f
Connie Gao <connieg@mit.edu>
2016-01-15 01:27:03 -0500

    Fixes to openbabel windows build

--- openbabel/bld.bat
+++ openbabel/bld.bat
@@ -1,20 +1,30 @@
-set CC=%PREFIX%\bin\gcc.exe
-set CXX=%PREFIX%\bin\g++.exe
-set F77=%PREFIX%\bin\gfortran.exe
-set F90=%PREFIX%\bin\gfortran.exe
+set CC=gcc
+set CXX=g++
+set F77=gfortran
+set F90=gfortran
+set CMAKE_CXX_COMPILER=g++
+set CMAKE_C_COMPILER=gcc
 
+copy "%PREFIX%\..\..\libs\libpython27.a" "%PREFIX%\libs\libpython27.a"
+echo %PREFIX%
+pip install -i https://pypi.anaconda.org/rmg/simple pycairo
+
+mkdir build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-      -DPYTHON_LIBRARY=%PREFIX%\libs\python27.lib ^
+      -DPYTHON_LIBRARY=%PREFIX%\libs\libpython27.lib ^
       -DPYTHON_EXECUTABLE=%PYTHON% ^
       -DPYTHON_INCLUDE_DIR=%PREFIX%\include ^
       -DPYTHON_BINDINGS=ON ^
       -DRUN_SWIG=ON ^
-	  -G "MinGW Makefiles"
+      -G "MinGW Makefiles" ..
 
+::-CMAKE_CXX_COMPILER=%PREFIX\Scripts\g++.exe ^
+ ::     -CMAKE_C_COMPILER=%PREFIX\Scripts\gcc.exe 
 :: false # pause here to see what's going on
 
-make -j%CPU_COUNT%
-make install
+mingw32-make -j%CPU_COUNT%
+mingw32-make install
 
 ::The python library and shared object do not install into site-packages so
 ::we put them there manually after the build.  This may be possible from CMake
