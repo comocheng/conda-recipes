d9b7ae80dd48ea0a230cee3294a9826c31ba7e6e
Connie Gao <connieg@mit.edu>
2016-01-29 20:42:29 -0500

    Make sure to use the first argument in mopac script

--- mopac/bld.bat
+++ mopac/bld.bat
@@ -4,5 +4,5 @@ xcopy /s %SRC_DIR% %PREFIX%\Scripts
 mkdir %PREFIX%\bin
 
 (echo set MOPAC_LICENSE=%PREFIX%\Scripts) >> %PREFIX%\Scripts\mopac.bat
-(echo %PREFIX%\Scripts\MOPAC2012.exe) >> %PREFIX%\Scripts\mopac.bat
+(echo %PREFIX%\Scripts\MOPAC2012.exe %%1) >> %PREFIX%\Scripts\mopac.bat
 chmod +x %PREFIX%\Scripts\mopac.bat
